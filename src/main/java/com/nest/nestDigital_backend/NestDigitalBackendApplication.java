b2aeadfaa6429000dec053f88bb347ab06c89247
sufailbadushat <sufailbadushat@gamil.com>
2022-12-12 11:56:23 +0530

    first commit

---
+++ src/main/java/com/nest/nestDigital_backend/NestDigitalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.nestDigital_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestDigitalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestDigitalBackendApplication.class, args);
+	}
+
+}
